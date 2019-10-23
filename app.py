3b8fac82710d9d992e3a4497f4d067b7b4c9c785
Dr-N0 <beckettjenen@gmail.com>
2019-10-23 14:05:59 -0400

    Final changes for Flask integration

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
-from flask import Flask, render_template, jsonify, request
+import os
+from flask import Flask
+from flask import render_template
+from flask import jsonify
+from flask import request
 
 app = Flask(__name__)
 
 @app.route('/')
-def index():
-  return render_template('index.html')
-
-@app.route('/event')
 def index():
   return render_template('index.html')
 
