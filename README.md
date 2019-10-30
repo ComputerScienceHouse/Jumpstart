9131ea2898ce9658b812be0e418a5089bee9ec12
Dr-N0 <beckettjenen@gmail.com>
2019-10-29 20:20:47 -0400

    Dark Mode! + non styled calendar integration

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # Jumpstart
 A graphical interface that runs on a RPI in the lobby of my dorm. Author: Beckett Jenen
+
+Run the following to setup:
+python3
+>>from app import db
+>>db.create_all()
+>>exit()
+
+python3 quickstart.py 
--- app.py
+++ app.py
@@ -1,13 +1,20 @@
+from __future__ import print_function
+import datetime
+import pickle
+import os.path
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
 from flask import *
 from json import *
-from flask_cors import CORS, cross_origin
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-CORS(app)
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
+# If modifying these scopes, delete the file token.pickle.
+SCOPES = ['https://www.googleapis.com/auth/calendar.readonly']
 
 class File(db.Model):
 	id = db.Column(db.Integer, primary_key=True)
@@ -19,6 +26,49 @@ class File(db.Model):
 @app.route('/')
 def index():
 	return render_template('index.html')
+
+@app.route('/calendar', methods=['GET'])
+def calendar():
+	"""Shows basic usage of the Google Calendar API.
+    Prints the start and name of the next 10 events on the user's calendar.
+    """
+	creds = None
+    # The file token.pickle stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+	if os.path.exists('token.pickle'):
+		with open('token.pickle', 'rb') as token:
+			creds = pickle.load(token)
+    # If there are no (valid) credentials available, let the user log in.
+	if not creds or not creds.valid:
+		if creds and creds.expired and creds.refresh_token:
+			creds.refresh(Request())
+		else:
+			flow = InstalledAppFlow.from_client_secrets_file('credentials.json', SCOPES)
+			creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+		with open('token.pickle', 'wb') as token:
+			pickle.dump(creds, token)
+
+	service = build('calendar', 'v3', credentials=creds)
+
+    # Call the Calendar API
+	now = datetime.datetime.utcnow().isoformat() + 'Z' # 'Z' indicates UTC time
+	events_result = service.events().list(calendarId='rti648k5hv7j3ae3a3rum8potk@group.calendar.google.com', timeMin=now,maxResults=10, singleEvents=True,orderBy='startTime').execute()
+	events = events_result.get('items', [])
+
+	finalEvents = ""
+
+	if not events:
+		print('No upcoming events found.')
+	for event in events:
+		start = event['start'].get('dateTime', event['start'].get('date'))
+		# finalEvents += start
+		finalEvents += ''.join(event['summary'])
+		finalEvents += "<br><br>"
+
+	eventList = {'data': finalEvents}
+	return jsonify(eventList)
 
 @app.route('/getdata-reset', methods=['GET', 'POST'])
 def add():
