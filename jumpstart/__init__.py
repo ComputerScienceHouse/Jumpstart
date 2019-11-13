ea821efa888d66a6e8328b2d345a9af0ac8de14b
Dr-N0 <beckettjenen@gmail.com>
2019-11-12 21:19:41 -0500

    Fin

--- jumpstart/__init__.py
+++ jumpstart/__init__.py
@@ -152,7 +152,7 @@ def showerthoughts():
 	randompost = random.randint(1,20)
 	url = requests.get('https://www.reddit.com/r/showerthoughts/hot.json', headers = {'User-agent': 'Showerthoughtbot 0.1'})
 	reddit = json.loads(url.text)
-	shower_thoughts = textwrap.fill((reddit['data']['children'][randompost]['data']['title']),50)
-	st = {'data': st}
+	shower_thoughts = textwrap.fill((reddit['data']['children'][randompost]['data']['title']),50).replaceAll("<.*?>", "")
+	st = {'data': shower_thoughts}
 	return jsonify(st)
 
